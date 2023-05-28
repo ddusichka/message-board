0250e3bb6c645b2251bcb407ed5a6c0893af9723
ddusichka <78527291+ddusichka@users.noreply.github.com>
2023-05-28 14:26:10 -0400

    condensing ProfilePage logic

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,75 +1,46 @@
 import { React, useState } from "react";
 import { auth } from "../firebase";
-import {
-  getAuth,
-  updateProfile,
-  updateEmail,
-  updatePassword,
-} from "firebase/auth";
+import { updateProfile, updateEmail, updatePassword } from "firebase/auth";
 import { db } from "../firebase";
 import { doc, setDoc, deleteDoc } from "firebase/firestore";
 
 const style = {
   appContainer: `flex-1 mx-auto ml-5 mt-5`,
-  sectionContainer: `flex flex-col mx-auto  bg-gray-100 shadow-xl border relative`,
   button: `bg-blue-500 hover:bg-blue-600 text-white  rounded h-10 w-24 text-center text-base block rounded cursor-pointer select-none mt-8`,
-  form: `bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4`,
   label: `block text-gray-700 text-sm font-bold mb-2 mt-4 text-left`,
   input: `shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`,
-  error: `text-red-500`,
-  flex: `flex`,
 };
 
 const ProfilePage = () => {
+  const user = auth.currentUser;
   const [initialName, setInitialName] = useState(auth.currentUser.displayName);
   const [initialEmail] = useState(auth.currentUser.email);
   const [name, setName] = useState(auth.currentUser.displayName);
   const [email, setEmail] = useState(auth.currentUser.email);
   const [newPassword, setNewPassword] = useState("");
 
-  function updateName() {
-    const auth = getAuth();
-    const user = auth.currentUser;
+  async function updateName() {
+    try {
+      if (initialName !== name) {
+        await updateProfile(user, { displayName: name, photoURL: null });
+        console.log("Name has been updated!");
+        setInitialName(name);
+        await setDoc(doc(db, "users", email), { name: name });
+      }
 
-    if (initialName !== name) {
-      updateProfile(user, {
-        displayName: name,
-        photoURL: null,
-      })
-        .then(() => {
-          console.log("Name has been updated!");
-          setInitialName(name);
-          setDoc(doc(db, "users", email), {
-            name: name,
-          });
-        })
-        .catch((error) => {
-          console.log(error.message);
-        });
-    }
+      if (initialEmail !== email) {
+        await updateEmail(user, email);
+        console.log("Email changed!");
+        await deleteDoc(doc(db, "users", initialEmail));
+        await setDoc(doc(db, "users", email), { name: name });
+      }
 
-    if (initialEmail !== email) {
-      updateEmail(user, email)
-        .then(() => {
-          console.log("Email changed!");
-          deleteDoc(doc(db, "users", initialEmail));
-          setDoc(doc(db, "users", email), {
-            name: name,
-          });
-        })
-        .catch((error) => {
-          console.log(error.message);
-        });
-    }
-
-    if (newPassword !== "") {
-      updatePassword(user, newPassword)
-        .then(() => {
-          console.log("Password successfully changed.");
-        })
-        .catch((error) => {
-          console.log(error.message);
-        });
+      if (newPassword !== "") {
+        await updatePassword(user, newPassword);
+        console.log("Password successfully changed.");
+      }
+    } catch (error) {
+      console.log(error.message);
     }
   }
 
